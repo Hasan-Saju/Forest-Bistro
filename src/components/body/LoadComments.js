f498b225b775534886d9df008c8beb6b187018e7
Hasan-Saju <hasansaju11@gmail.com>
2021-07-23 22:26:45 +0600

    LoadComments Component

---
+++ src/components/body/LoadComments.js
@@ -0,0 +1,16 @@
+import React from "react";
+import dateformat from "dateformat";
+
+const LoadComments = (props) => {
+  return props.comments.map((comment) => {
+    return (
+      <div key={comment.id}>
+        <h5>{comment.author}</h5>
+        <p>{comment.comment}</p>
+        <p>{dateformat(comment.date, "dddd, mmmm dS, yyyy, h:MM TT")}</p>
+      </div>
+    );
+  });
+};
+
+export default LoadComments;
