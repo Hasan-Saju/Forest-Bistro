1c3fcde564663ec49f43b61839bd7b4ba23cbe4d
Hasan-Saju <hasansaju11@gmail.com>
2021-07-27 12:38:35 +0600

    Footer Component Added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>
+              Forest Bistro &#169; Copyright 2021, HS Software Solutions Ltd.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
