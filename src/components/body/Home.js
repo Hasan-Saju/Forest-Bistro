c31b9212e0c49a75f86ce40c5b718475cdf3d330
Hasan-Saju <hasansaju11@gmail.com>
2021-07-28 13:44:56 +0600

    Create store,access redux frm comp,dispatch action

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -3,16 +3,25 @@ import { connect } from "react-redux";
 
 const mapStateToProps = (state) => {
   // get all the state of reducer here
-  console.log("map state to props: ", state);
+  // console.log("map state to props: ", state);
   return {
     dishes: state.dishes, //will get dishes as props in Home
+    sample: state.sample,
   };
 };
 
 class Home extends Component {
   componentDidMount() {
-    console.log("Home State: ", this.state);
+    // console.log("Home State: ", this.state);
     console.log("Home Props: ", this.props);
+    this.props.dispatch({
+      type: "TEST",
+      str: "Bangladesh",
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("Home Props Update: ", this.props);
   }
   render() {
     document.title = "Forest Bistro";
