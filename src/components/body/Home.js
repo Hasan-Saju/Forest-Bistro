c82c622cbf0a4967907d4c52a544a032e98da343
Hasan-Saju <hasansaju11@gmail.com>
2021-07-28 12:24:58 +0600

    Accessing Redux from components

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => {
+  // get all the state of reducer here
+  console.log("map state to props: ", state);
+  return {
+    dishes: state.dishes, //will get dishes as props in Home
+  };
+};
 
 class Home extends Component {
+  componentDidMount() {
+    console.log("Home State: ", this.state);
+    console.log("Home Props: ", this.props);
+  }
   render() {
     document.title = "Forest Bistro";
     return <div></div>;
   }
 }
 
-export default Home;
+export default connect(mapStateToProps)(Home);
