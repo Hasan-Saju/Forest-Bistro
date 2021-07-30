122af50f0abf30a2f27aba71a9058e0c6764d00a
Hasan-Saju <hasansaju11@gmail.com>
2021-07-30 16:32:49 +0600

    Applying Middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Reducer } from "./reducer";
+import logger from "redux-logger";
 
-const myStore = createStore(Reducer);
+// logger helps to demonstrate the change of reducer in every step
+const myStore = createStore(Reducer, applyMiddleware(logger));
 
 export default myStore;
