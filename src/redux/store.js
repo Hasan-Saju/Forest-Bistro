d7f679241f5f7c316e50afeb8b7abc1e2fae0cf0
Hasan-Saju <hasansaju11@gmail.com>
2021-07-30 18:09:15 +0600

    Redux Thunk for Async Code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import { Reducer } from "./reducer";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 // logger helps to demonstrate the change of reducer in every step
-const myStore = createStore(Reducer, applyMiddleware(logger));
+const myStore = createStore(Reducer, applyMiddleware(logger, thunk));
 
 export default myStore;
